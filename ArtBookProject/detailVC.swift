27ce79b3d782696c5c3722dac62aabefe503d5d6
Oğuz ÖZGÜL <oguzhannet@gmail.com>
2020-04-26 21:29:32 +0300

    no message

--- ArtBookProject/detailVC.swift
+++ ArtBookProject/detailVC.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class detailVC: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func savebuttonClicked(_ sender: Any) {
+        
     }
 }

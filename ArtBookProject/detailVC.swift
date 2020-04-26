5bb9d69db1122b7b34a7d4696245f505f2a05465
Oğuz ÖZGÜL <oguzhannet@gmail.com>
2020-04-26 21:16:19 +0300

    detailVc eklendi

---
+++ ArtBookProject/detailVC.swift
@@ -0,0 +1,18 @@
+//
+//  detailVC.swift
+//  ArtBookProject
+//
+//  Created by Oğuz Özgül on 26.04.2020.
+//  Copyright © 2020 Oğuz Özgül. All rights reserved.
+//
+
+import UIKit
+
+class detailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
